9abab7bd58219358715c55f34ff8615d819283f2
jeloneklibera <jan.piotrowski00@gmail.com>
2018-12-18 12:17:18 +0100

    nietoperze AI

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ function preload() {
     this.load.image('ground', 'assets/platform.png');
     this.load.image('jupiter', 'assets/Jupiter.png');
     this.load.image('diamond', 'assets/diament.png');
+    this.load.image('portal','assets/doors.png');
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     this.load.spritesheet('bat', 'assets/bat_sprite.png', { frameWidth: 64, frameHeight: 64 });
 }
@@ -47,6 +48,11 @@ function create() {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+    //doors = this.physics.add.group();
+    //var door = doors.create(200,400,'portal');
+    //door.setScale(0.2);
+     
+    console.log(platforms);
 
     player = this.physics.add.sprite(100, 450, 'dude');
     player.setBounce(0.2);
@@ -80,6 +86,7 @@ function create() {
     });
 
     cursors = this.input.keyboard.createCursorKeys();
+    
 
     diamonds = this.physics.add.group({
         key: 'diamond',
@@ -91,19 +98,70 @@ function create() {
         child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.3));
         child.setScale(0.1);
     });
-
     bats = this.physics.add.group();
+    var bat = bats.create(16, 150, 'bat');
+    var bat2 = bats.create(500, 130, 'bat');
+    bat.setCollideWorldBounds(true);
+    bat.setBounceY(0);
+    bat.setGravityY(0.3);
+    bat.setBounceX(0.9); 
+    bat.setVelocityX(150);
+    bat.anims.play('move', true);
+
+    bat2.setCollideWorldBounds(true);
+    bat2.setBounceY(0);
+    bat2.setGravityY(0.3);
+    bat2.setBounceX(0.9); 
+    bat2.setVelocityX(190);
+    bat2.anims.play('move', true);
+   
 
 
+
+
+    /*  var bat = bats.create(16, 16, 'bat');
+         bat.setBounce(0.8);
+         bat.setCollideWorldBounds(true);
+         if (bat.x = 10)
+         bat.setVelocity(Phaser.Math.Between(0, 200), 20);
+         else
+         bat.setVelocity(Phaser.Math.Between(-200, 0), 20);
+         bat.anims.play('move', true);
+         bat.allowGravity = false; */
 
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: 'white' });
-    this.physics.add.collider(bats, platforms);
+    /* bats.prototype.update = function () {
+  
+         game.physics.arcade.collide(this, platforms, function (bats, platform) {
+             // if slime is moving to the right, 
+             // check if its position greater than the width of the platform minus its width
+             // if slime is moving to the left, 
+             // check if its position exceeds the left-most point of the platform
+             if (bats.body.velocity.x > 0 && bats.x > platform.x + (platform.width - bats.width) ||
+                     bats.body.velocity.x < 0 && bats.x < platform.x) {
+                 bats.body.velocity.x *= -1; 
+             } 
+             if (bats.body.velocity.x > 0) {
+                 bats.animations.play('move');
+             } else {
+                 bats.animations.play('move');
+             }
+         });
+      
+         game.physics.arcade.collide(this, bats, function (bat, bats) {
+             bat.body.velocity.x *= -1.0001;
+         });
+      
+     }; */
+    
+     
+   
     this.physics.add.collider(bats, player, hitBat, null, this);
     this.physics.add.collider(player, platforms);
+    this.physics.add.collider(bats,platforms,getback,null,this);
     this.physics.add.collider(diamonds, platforms);
-
     this.physics.add.overlap(player, diamonds, collectDiamond, null, this);
-
+    
 
     //  this.add.image(400,300,'jupiter');
 
@@ -129,51 +187,67 @@ function update() {
     if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-330);
     }
-
+    
 }
 
 function collectDiamond(player, diamond) {
     diamond.disableBody(true, true);
-
     score += 10;
     scoreText.setText('Score: ' + score);
 
-    if (diamonds.countActive(true) === 0) {
-        diamonds.children.iterate(function (child) {
-            child.enableBody(true, child.x, 0, true, true);
-        });
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-        var bat = bats.create(x, 16, 'bat');
-        bat.setBounce(1);
-        bat.setCollideWorldBounds(true);
-        bat.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bat.anims.play('move', true);
-        bat.allowGravity = false;
+    //  if (diamonds.countActive(true) === 0) {
+    //    diamonds.children.iterate(function (child) {
+    //      child.enableBody(true, child.x, 0, true, true);
+    //});
+    //var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+    //    var bat = bats.create(x, 16, 'bat');
+    //  bat.setBounce(1);
+    //bat.setCollideWorldBounds(true);
+    //bat.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    //bat.anims.play('move', true);
+    //bat.allowGravity = false;
 
-    }
+    //}
+
+
 }
 
+
+
+
+function getback(bat, platforms) {
+
+    // if slime is moving to the right, 
+    // check if its position greater than the width of the platform minus its width
+    // if slime is moving to the left, 
+    // check if its position exceeds the left-most point of the platform
+    
+    if (bat.body.velocity.x > 0 && (bat.x + 200) > (platforms.x + platforms.width) ||
+            (bat.body.velocity.x< 0 && bat.x < platforms.x -200)) {
+        bat.setVelocityX(bat.body.velocity.x * -1.1);
+        
+    } 
+}
 
 function hitBat(player, bat) {
     this.physics.pause();
     player.setTint(0xff0000);
     player.anims.play('turn');
     gameOver = true;
-   game.scene.add('Highscore',Highscore,true,{x:0,y:0});
-   this.input.keyboard.enabled = false;
+    game.scene.add('Highscore', Highscore, true, { x: 0, y: 0 });
+    this.input.keyboard.enabled = false;
 
 }
 
 class InputPanel extends Phaser.Scene {
 
-    constructor ()
-    {
+    constructor() {
         super({ key: 'InputPanel', active: false });
 
         this.chars = [
-            [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' ],
-            [ 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T' ],
-            [ 'U', 'V', 'W', 'X', 'Y', 'Z', '.', '-', '<', '>' ]
+            ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+            ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'],
+            ['U', 'V', 'W', 'X', 'Y', 'Z', '.', '-', '<', '>']
         ];
 
         this.cursor = new Phaser.Math.Vector2();
@@ -185,8 +259,7 @@ class InputPanel extends Phaser.Scene {
         this.charLimit = 3;
     }
 
-    create ()
-    {
+    create() {
         let text = this.add.bitmapText(130, 50, 'arcade', 'ABCDEFGHIJ\n\nKLMNOPQRST\n\nUVWXYZ.-');
 
         text.setLetterSpacing(20);
@@ -210,11 +283,10 @@ class InputPanel extends Phaser.Scene {
         text.on('pointermove', this.moveBlock, this);
         text.on('pointerup', this.pressKey, this);
 
-        
+
     }
 
-    moveBlock (pointer, x, y)
-    {
+    moveBlock(pointer, x, y) {
         let cx = Phaser.Math.Snap.Floor(x, 52, 0, true);
         let cy = Phaser.Math.Snap.Floor(y, 64, 0, true);
         let char = this.chars[cy][cx];
@@ -225,85 +297,68 @@ class InputPanel extends Phaser.Scene {
         this.block.y = this.text.y - 2 + (cy * 64);
     }
 
-    moveLeft ()
-    {
-        if (this.cursor.x > 0)
-        {
+    moveLeft() {
+        if (this.cursor.x > 0) {
             this.cursor.x--;
             this.block.x -= 52;
         }
-        else
-        {
+        else {
             this.cursor.x = 9;
             this.block.x += 52 * 9;
         }
     }
 
-    moveRight ()
-    {
-        if (this.cursor.x < 9)
-        {
+    moveRight() {
+        if (this.cursor.x < 9) {
             this.cursor.x++;
             this.block.x += 52;
         }
-        else
-        {
+        else {
             this.cursor.x = 0;
             this.block.x -= 52 * 9;
         }
     }
 
-    moveUp ()
-    {
-        if (this.cursor.y > 0)
-        {
+    moveUp() {
+        if (this.cursor.y > 0) {
             this.cursor.y--;
             this.block.y -= 64;
         }
-        else
-        {
+        else {
             this.cursor.y = 2;
             this.block.y += 64 * 2;
         }
     }
 
-    moveDown ()
-    {
-        if (this.cursor.y < 2)
-        {
+    moveDown() {
+        if (this.cursor.y < 2) {
             this.cursor.y++;
             this.block.y += 64;
         }
-        else
-        {
+        else {
             this.cursor.y = 0;
             this.block.y -= 64 * 2;
         }
     }
 
-    anyKey (event)
-    {
+    anyKey(event) {
         //  Only allow A-Z . and -
 
         let code = event.keyCode;
 
-        if (code === Phaser.Input.Keyboard.KeyCodes.PERIOD)
-        {
+        if (code === Phaser.Input.Keyboard.KeyCodes.PERIOD) {
             this.cursor.set(6, 2);
             this.pressKey();
         }
-        else if (code === Phaser.Input.Keyboard.KeyCodes.MINUS)
-        {
+        else if (code === Phaser.Input.Keyboard.KeyCodes.MINUS) {
             this.cursor.set(7, 2);
             this.pressKey();
         }
-        else if (code === Phaser.Input.Keyboard.KeyCodes.BACKSPACE || code === Phaser.Input.Keyboard.KeyCodes.DELETE)
-        {
+        else if (code === Phaser.Input.Keyboard.KeyCodes.BACKSPACE || code === Phaser.Input.Keyboard.KeyCodes.DELETE) {
             this.cursor.set(8, 2);
             this.pressKey();
         }
-        else if (code >= Phaser.Input.Keyboard.KeyCodes.A && code <= Phaser.Input.Keyboard.KeyCodes.Z)
-        {
+        else if (code >= Phaser.Input.Keyboard.KeyCodes.A && code <= Phaser.Input.Keyboard.KeyCodes.Z) {
             code -= 65;
 
             let y = Math.floor(code / 10);
@@ -314,8 +369,7 @@ class InputPanel extends Phaser.Scene {
         }
     }
 
-    pressKey ()
-    {
+    pressKey() {
         let x = this.cursor.x;
         let y = this.cursor.y;
         let nameLength = this.name.length;
@@ -323,20 +377,17 @@ class InputPanel extends Phaser.Scene {
         this.block.x = this.text.x - 10 + (x * 52);
         this.block.y = this.text.y - 2 + (y * 64);
 
-        if (x === 9 && y === 2 && nameLength > 0)
-        {
+        if (x === 9 && y === 2 && nameLength > 0) {
             //  Submit
             this.events.emit('submitName', this.name);
         }
-        else if (x === 8 && y === 2 && nameLength > 0)
-        {
+        else if (x === 8 && y === 2 && nameLength > 0) {
             //  Rub
             this.name = this.name.substr(0, nameLength - 1);
 
             this.events.emit('updateName', this.name);
         }
-        else if (this.name.length < this.charLimit)
-        {
+        else if (this.name.length < this.charLimit) {
             //  Add
             this.name = this.name.concat(this.chars[y][x]);
 
@@ -347,8 +398,7 @@ class InputPanel extends Phaser.Scene {
 
 class Starfield extends Phaser.Scene {
 
-    constructor ()
-    {
+    constructor() {
         super({ key: 'Starfield', active: true });
 
         this.stars;
@@ -362,20 +412,17 @@ class Starfield extends Phaser.Scene {
         this.zz = [];
     }
 
-    preload ()
-    {
+    preload() {
         this.load.image('star', 'assets/star4.png');
     }
 
-    create ()
-    {
+    create() {
         //  Do this, otherwise this Scene will steal all keyboard input
         this.input.keyboard.enabled = false;
 
         this.stars = this.add.blitter(0, 0, 'star');
 
-        for (let i = 0; i < this.max; i++)
-        {
+        for (let i = 0; i < this.max; i++) {
             this.xx[i] = Math.floor(Math.random() * 800) - 400;
             this.yy[i] = Math.floor(Math.random() * 600) - 300;
             this.zz[i] = Math.floor(Math.random() * 1700) - 100;
@@ -388,18 +435,15 @@ class Starfield extends Phaser.Scene {
         }
     }
 
-    update (time, delta)
-    {
-        for (let i = 0; i < this.max; i++)
-        {
+    update(time, delta) {
+        for (let i = 0; i < this.max; i++) {
             let perspective = this.distance / (this.distance - this.zz[i]);
             let x = 400 + this.xx[i] * perspective;
             let y = 300 + this.yy[i] * perspective;
 
             this.zz[i] += this.speed * (delta / 1000);
 
-            if (this.zz[i] > 300)
-            {
+            if (this.zz[i] > 300) {
                 this.zz[i] -= 600;
             }
 
@@ -414,15 +458,13 @@ class Starfield extends Phaser.Scene {
 
 class Highscore extends Phaser.Scene {
 
-    constructor ()
-    {
+    constructor() {
         super({ key: 'Highscore', active: true });
 
         this.playerText;
     }
 
-    preload ()
-    {
+    preload() {
         this.load.image('block', 'assets/block.png');
         this.load.image('rub', 'assets/rub.png');
         this.load.image('end', 'assets/end.png');
@@ -430,16 +472,15 @@ class Highscore extends Phaser.Scene {
         this.load.bitmapFont('arcade', 'assets/fonts/bitmap/arcade.png', 'assets/fonts/bitmap/arcade.xml');
     }
 
-    create ()
-    {
+    create() {
         this.add.bitmapText(100, 260, 'arcade', 'RANK  SCORE   NAME').setTint(0xff00ff);
-        this.add.bitmapText(100, 310, 'arcade', '1ST   50000').setTint(0xff0000);
+        this.add.bitmapText(100, 310, 'arcade', '1ST   ' + score).setTint(0xff0000);
 
         this.playerText = this.add.bitmapText(580, 310, 'arcade', '').setTint(0xff0000);
 
         //  Do this, otherwise this Scene will steal all keyboard input
         this.input.keyboard.enabled = true;
-        game.scene.add('InputPanel',InputPanel,true,{x:0,y:0});
+        game.scene.add('InputPanel', InputPanel, true, { x: 0, y: 0 });
         this.scene.launch('InputPanel');
 
         let panel = this.scene.get('InputPanel');
@@ -449,18 +490,16 @@ class Highscore extends Phaser.Scene {
         panel.events.on('submitName', this.submitName, this);
     }
 
-    submitName ()
-    {
+    submitName() {
         this.scene.stop('InputPanel');
 
-        this.add.bitmapText(100, 360, 'arcade', '2ND   40000    ANT').setTint(0xff8200);
-        this.add.bitmapText(100, 410, 'arcade', '3RD   30000    .-.').setTint(0xffff00);
-        this.add.bitmapText(100, 460, 'arcade', '4TH   20000    BOB').setTint(0x00ff00);
-        this.add.bitmapText(100, 510, 'arcade', '5TH   10000    ZIK').setTint(0x00bfff);
+        this.add.bitmapText(100, 360, 'arcade', '2ND   ' + 40 + '       ANT').setTint(0xff8200);
+        this.add.bitmapText(100, 410, 'arcade', '3RD   ' + 30 + '       .-.').setTint(0xffff00);
+        this.add.bitmapText(100, 460, 'arcade', '4TH   ' + 20 + '       BOB').setTint(0x00ff00);
+        this.add.bitmapText(100, 510, 'arcade', '5TH   ' + 10 + '       ZIK').setTint(0x00bfff);
     }
 
-    updateName (name)
-    {
+    updateName(name) {
         this.playerText.setText(name);
     }
 
